f1bd1a06bb8b0db7f82280a9b08ece275585fa47
HarshaMacherla <macherlaharsha997@gmail.com>
2023-07-18 22:44:44 +0530

    Added login page with Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from "react";
 import "./App.css";
-import UserRegistration from "./components/Registration/UserRegistration";
+import SignUpPage from "./components/UserAuthentication/SignUpPage";
+import LoginPage from "./components/UserAuthentication/LoginPage";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(
+    !!localStorage.getItem("token")
+  );
+
+  const [isLogin, setIsLogin] = React.useState(true);
+
   return (
     <>
-      <UserRegistration />
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn && <Home />}
+          {!isLoggedIn && <Redirect to="/login" />}
+        </Route>
+        <Route path="/" exact>
+          {isLoggedIn && <Home />}
+          {!isLoggedIn && <Redirect to="/login" />}
+        </Route>
+        {isLogin ? (
+          <Route path="/login">
+            {isLoggedIn && <Redirect to="/" />}
+            {!isLoggedIn && (
+              <LoginPage
+                setIsLogin={setIsLogin}
+                setIsLoggedIn={setIsLoggedIn}
+              />
+            )}
+          </Route>
+        ) : (
+          <Route path="/signup">
+            {isLoggedIn && <Redirect to="/" />}
+            {!isLoggedIn && (
+              <SignUpPage
+                setIsLogin={setIsLogin}
+                setIsLoggedIn={setIsLoggedIn}
+              />
+            )}
+          </Route>
+        )}
+      </Switch>
     </>
   );
 };
---
+++ src/components/UserAuthentication/SignUpPage.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+
+const SignUpPage = ({ setIsLogin, setIsLoggedIn }) => {
+  const emailRef = React.useRef();
+  const passwordRef = React.useRef();
+  const confirmPasswordRef = React.useRef();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+
+    if (password.trim().length !== confirmPassword.trim().length) {
+      alert("Passwords mismatch!");
+      return;
+    }
+
+    if (password.trim().length === 0 || confirmPassword.trim().length === 0) {
+      alert("Password cannot be empty");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAvBSC-wnMSr4LTyhMGqXtQdczeBxPzacw",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email,
+            password,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        throw new Error(errorResponse.error.message);
+      }
+      const data = await response.json();
+      localStorage.setItem("token", data.idToken);
+      console.log("SingedUp Successfully");
+      setIsLoggedIn(true);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      <Form
+        onSubmit={handleSubmit}
+        className="mx-auto my-5 p-5 bg-light border rounded"
+        style={{ width: "400px" }}
+      >
+        <h2 className="text-center mb-4">SignUp</h2>
+
+        <Form.Control type="email" placeholder="Email" ref={emailRef} />
+        <br />
+
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          ref={passwordRef}
+        />
+        <br />
+
+        <Form.Control
+          type="password"
+          placeholder="Confirm Password"
+          ref={confirmPasswordRef}
+        />
+        <br />
+
+        <div className="d-grid gap-2">
+          <button className="btn btn-primary" type="submit">
+            Sign up
+          </button>
+        </div>
+      </Form>
+
+      <div className="text-center">
+        <Button
+          variant="secondary"
+          className="mx-auto text-center border rounded p-2 d-block"
+          style={{ width: "400px" }}
+          onClick={() => {
+            setIsLogin(true);
+          }}
+        >
+          Have an account? Login
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default SignUpPage;
